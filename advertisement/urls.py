4c2b21fed16f73abbad299d6093b9969e0f27f87
Woldeamanual <woldeamanual@addwebsolution.in>
2023-03-24 09:53:29 +0300

    Add urls for the detail, update and delete functionality

--- advertisement/urls.py
+++ advertisement/urls.py
@@ -4,7 +4,11 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.AdvertisementListCreateAPIView.as_view()),
-    path('<int:pk>/update/', views.AdvertisementUpdateAPIView.as_view())
+    path('<int:pk>/', views.AdvertisementDetailsAPIView.as_view(),
+         name='advertisement-detail'),
+    path('<int:pk>/update/', views.AdvertisementUpdateAPIView.as_view()),
+    path('<int:pk>/delete/', views.AdvertisementDeleteAPIView.as_view())
+
 
 
 
