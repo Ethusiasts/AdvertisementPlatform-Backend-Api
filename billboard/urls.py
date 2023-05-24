b3d96f0d4135678215427e06fe779ffe3fed396f
biruk ayalew <se.biruk.ayalew@gmail.com>
2023-05-24 14:36:37 +0300

    add search endpoint

--- billboard/urls.py
+++ billboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from billboard.views import Billboards, BillboardDetail
+from billboard.views import Billboards, BillboardDetail, SearchBillboards
 
 urlpatterns = [
     path('', Billboards.as_view(), name='billboards'),
-    path('<int:id>', BillboardDetail.as_view(), name='billboard')
+    path('<int:id>', BillboardDetail.as_view(), name='billboard'),
+    path('search', SearchBillboards.as_view(), name='search')
+
 ]
