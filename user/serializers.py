ac597ee97296cb2aec5f71e221ae572731cf616e
biruk ayalew <se.biruk.ayalew@gmail.com>
2023-06-02 22:09:23 +0300

    remove is_verified from serializer

--- user/serializers.py
+++ user/serializers.py
@@ -6,7 +6,7 @@ from user.models import User, UserProfile
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'role', 'is_verified']
+        fields = ['id', 'email', 'role']
         extra_kwargs = {'password': {'write_only': True}}
 
 
