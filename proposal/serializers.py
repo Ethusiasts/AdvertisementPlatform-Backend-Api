ca7a9f8fa381301b9e863a9bdf68e3b878f06a76
biruk ayalew <se.biruk.ayalew@gmail.com>
2023-06-03 23:03:57 +0300

    populate reference ids  in proposal

--- proposal/serializers.py
+++ proposal/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
+from advertisement.serializers import AdvertisementSerializer
 from billboard.serializers import BillboardSerializer
+from media_agency.serializers import MediaAgencySerializer
 from proposal.models import Proposal
+from user.serializers import UserSerializer
 
 
 class ProposalSerializer(serializers.ModelSerializer):
@@ -12,6 +15,9 @@ class ProposalSerializer(serializers.ModelSerializer):
 
 class ProposalDetailSerializer(serializers.ModelSerializer):
     billboard_id = BillboardSerializer()
+    user_id = UserSerializer()
+    media_agency_id = MediaAgencySerializer()
+    advertisement_id = AdvertisementSerializer()
 
     class Meta:
         model = Proposal
