c389de9290731c6fa5254fea3a1256b93c3090f5
biruk ayalew <se.biruk.ayalew@gmail.com>
2023-06-15 15:46:24 +0300

    add multiple proposal feature

--- proposal/urls.py
+++ proposal/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from proposal.views import ProposalDetail, Proposals
+from proposal.views import MultipleProposals, ProposalDetail, Proposals
 
 urlpatterns = [
     path('', Proposals.as_view()),
+    path('multiple', MultipleProposals.as_view()),
     path('<int:id>', ProposalDetail.as_view())
+
 ]
--- proposal/views.py
+++ proposal/views.py
@@ -1,10 +1,12 @@
+from decimal import Decimal
 from django.shortcuts import render
 from rest_framework import generics
+from advertisement.models import Advertisement
 from advertisement_platform.errors import error_500, success_200, success_201, error_400, error_404, success_204
 from proposal.models import Proposal
 from rest_framework.pagination import PageNumberPagination
 
-from proposal.serializers import ProposalDetailSerializer, ProposalGetSerializer, ProposalPostSerializer
+from proposal.serializers import MultipleProposalSerializer, ProposalDetailSerializer, ProposalGetSerializer, ProposalPostSerializer
 # Create your views here.
 
 
@@ -38,6 +40,59 @@ class Proposals(generics.GenericAPIView):
             return success_201('successfully created', serializer.data)
         print(serializer.errors)
         return error_400(serializer.errors)
+
+
+class MultipleProposals(generics.GenericAPIView):
+    serializer_class = MultipleProposalSerializer
+
+    def post(self, request):
+        try:
+            medias_data = request.data.get("medias", [])
+            proposal = {}
+            advertisement = Advertisement.objects.get(
+                id=request.data['advertisement_id'])
+            for media_data in medias_data:
+                if 'daily_rate_per_sq' in media_data:
+                    width = advertisement.width
+                    height = advertisement.height
+                    daily_rate_per_sq = media_data.get('daily_rate_per_sq')
+                    total_price = round(
+                        width * height * Decimal(daily_rate_per_sq), 2)
+                    proposal = {
+                        "name": request.data['name'],
+                        "user_id": request.data['user_id'],
+                        "billboard_id": media_data.get('id'),
+                        "media_agency_id": media_data.get('media_agency_id'),
+                        "advertisement_id": request.data['advertisement_id'],
+                        "description": request.data['description'],
+                        "production": request.data['production'],
+                        "total_price": total_price
+                    }
+                else:
+                    duration = advertisement.duration_in_hour
+                    normal = Decimal(media_data.get('normal'))
+                    total_price = round(duration * 60 * normal, 2)
+                    proposal = {
+                        "name": request.data['name'],
+                        "user_id": request.data['user_id'],
+                        "agency_id": media_data.get('id'),
+                        "media_agency_id": media_data.get('media_agency_id'),
+                        "advertisement_id": request.data['advertisement_id'],
+                        "description": request.data['description'],
+                        "production": request.data['production'],
+                        "total_price": total_price
+                    }
+                serializer = ProposalPostSerializer(data=proposal)
+                if serializer.is_valid():
+                    serializer.save()
+                else:
+                    print(serializer.errors)
+                    return error_400(serializer.errors)
+            return success_201('successfully created', '')
+
+        except Exception as e:
+            print(e)
+            return error_400(e)
 
 
 class ProposalDetail(generics.GenericAPIView):
